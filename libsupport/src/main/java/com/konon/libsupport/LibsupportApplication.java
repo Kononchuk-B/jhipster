948c68ee1f3bcfa7bbe32373247bcdccbb6a7673
Bohdan Kononchuk <Bohdan Kononchuk>
2017-01-03 15:16:41 +0200

    Updated app to initial state

---
+++ libsupport/src/main/java/com/konon/libsupport/LibsupportApplication.java
@@ -0,0 +1,12 @@
+package com.konon.libsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibsupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibsupportApplication.class, args);
+	}
+}
