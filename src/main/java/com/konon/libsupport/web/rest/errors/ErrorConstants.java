2e2438ade8d27b487dda4d0959edea36d09dd242
Bohdan Kononchuk <Bohdan Kononchuk>
2017-01-27 17:44:28 +0200

    Feedback for current user

--- src/main/java/com/konon/libsupport/web/rest/errors/ErrorConstants.java
+++ src/main/java/com/konon/libsupport/web/rest/errors/ErrorConstants.java
@@ -2,11 +2,11 @@ package com.konon.libsupport.web.rest.errors;
 
 public final class ErrorConstants {
 
-    public static final String ERR_CONCURRENCY_FAILURE = "error.concurrencyFailure";
-    public static final String ERR_ACCESS_DENIED = "error.accessDenied";
-    public static final String ERR_VALIDATION = "error.validation";
-    public static final String ERR_METHOD_NOT_SUPPORTED = "error.methodNotSupported";
-    public static final String ERR_INTERNAL_SERVER_ERROR = "error.internalServerError";
+    public static final String ERR_CONCURRENCY_FAILURE = "Error : Concurrency Failure";
+    public static final String ERR_ACCESS_DENIED = "Error : Access Denied";
+    public static final String ERR_VALIDATION = "Error : Validation";
+    public static final String ERR_METHOD_NOT_SUPPORTED = "Error : Method Not Supported";
+    public static final String ERR_INTERNAL_SERVER_ERROR = "Error : Internal Server Error";
 
     private ErrorConstants() {
     }
--- src/main/webapp/app/entities/feedback/feedback-dialog.controller.js
+++ src/main/webapp/app/entities/feedback/feedback-dialog.controller.js
@@ -1,32 +1,47 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('libSupportApp')
         .controller('FeedbackDialogController', FeedbackDialogController);
 
-    FeedbackDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'Feedback', 'Book', 'User'];
+    FeedbackDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'Feedback', 'Book', 'User', 'Principal'];
 
-    function FeedbackDialogController ($timeout, $scope, $stateParams, $uibModalInstance, entity, Feedback, Book, User) {
+    function FeedbackDialogController($timeout, $scope, $stateParams, $uibModalInstance, entity, Feedback, Book, User, Principal) {
         var vm = this;
 
         vm.feedback = entity;
         vm.clear = clear;
-        vm.datePickerOpenStatus = {};
-        vm.openCalendar = openCalendar;
         vm.save = save;
         vm.books = Book.query();
         vm.users = User.query();
+        vm.today = new Date();
+        vm.currentUser = load;
 
-        $timeout(function (){
+        $timeout(function () {
             angular.element('.form-group:eq(1)>input').focus();
         });
 
-        function clear () {
+        function clear() {
             $uibModalInstance.dismiss('cancel');
         }
 
-        function save () {
+        function load(login) {
+            User.get({login: login}, function (result) {
+                vm.user = result;
+            });
+        }
+
+        Principal.identity().then(function (account) {
+            User.get({login: account.login}, function (result) {
+                vm.currentUser = result;
+            });
+        });
+
+        function save() {
+            console.log(vm.feedback.user);
+            console.log(vm.currentUser);
+            vm.feedback.user = vm.currentUser;
             vm.isSaving = true;
             if (vm.feedback.id !== null) {
                 Feedback.update(vm.feedback, onSaveSuccess, onSaveError);
@@ -35,20 +50,15 @@
             }
         }
 
-        function onSaveSuccess (result) {
+        function onSaveSuccess(result) {
             $scope.$emit('libSupportApp:feedbackUpdate', result);
             $uibModalInstance.close(result);
             vm.isSaving = false;
         }
 
-        function onSaveError () {
+        function onSaveError() {
             vm.isSaving = false;
         }
 
-        vm.datePickerOpenStatus.dateOfPublish = false;
-
-        function openCalendar (date) {
-            vm.datePickerOpenStatus[date] = true;
-        }
     }
 })();
