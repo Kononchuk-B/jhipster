4aedc060a9ab7bb9f14860e90999a443a147fd02
Bohdan Kononchuk <Bohdan Kononchuk>
2017-01-30 18:41:10 +0200

    Feedback's form edited

--- src/main/java/com/konon/libsupport/domain/Feedback.java
+++ src/main/java/com/konon/libsupport/domain/Feedback.java
@@ -33,8 +33,8 @@ public class Feedback implements Serializable {
     @Column(name = "date_of_publish", nullable = false)
     private LocalDate dateOfPublish;
 
-    @Size(min = 10, max = 255)
-    @Column(name = "description", length = 255)
+    @Size(min = 10, max = 1000)
+    @Column(name = "description", length = 1000)
     private String description;
 
     @ManyToOne
